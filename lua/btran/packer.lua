c0ee80acff39636ec60e61d7008793d2f32bc756
Bach Tran <bachn.tran@outlook.com>
2024-10-24 18:38:14 -0400

    Try using fugitive to commit this commit

--- lua/btran/packer.lua
+++ lua/btran/packer.lua
@@ -19,5 +19,6 @@ use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 use('nvim-treesitter/playground') 
 use('theprimeagen/harpoon')
 use('mbbill/undotree')
+use('tpope/vim-fugitive')
 end)
 
